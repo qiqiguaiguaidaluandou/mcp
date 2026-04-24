4218b13244f6ff84c8b5db518820025262e38de9
qiqiguaiguaidaluandou <2280820483@qq.com>
2026-04-24 15:19:26 +0800

    更改ip

--- src/mcpserver/config.py
+++ src/mcpserver/config.py
@@ -1,6 +1,6 @@
 import os
 
-HOST = os.getenv("MCP_HOST", "127.10.42.142")
+HOST = os.getenv("MCP_HOST", "172.10.42.142")
 PORT = int(os.getenv("MCP_PORT", "8765"))
 
 MY_API_BASE_URL = os.getenv("MY_API_BASE_URL", "https://httpbin.org")
